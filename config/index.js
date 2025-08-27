58f64d7aac54ac5507f7c0dfe23bdc08593e5aaa
刘胜 <black.liusheng@gmail.com>
2025-08-28 00:36:11 +0800

    🐳 chore: update ci build

--- config/index.js
+++ config/index.js
@@ -7,7 +7,10 @@ module.exports = {
     index: path.resolve(__dirname, '../dist/index.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: process.env.NODE_ENV === 'production' ? '/flyio-js/' : './',
+    // self server
+    // assetsPublicPath: process.env.NODE_ENV === 'production' ? '/flyio-js/' : './',
+    // vercel server
+    assetsPublicPath: process.env.NODE_ENV === 'production' ? '/' : './',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
